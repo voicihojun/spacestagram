25aabf2b0d30238e5eb97ef4d85427a49c967476
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2022-01-16 15:19:44 -0500

    complete requirements and  all options

--- README.md
+++ README.md
@@ -1,70 +1,95 @@
-# Getting Started with Create React App
+Technical requirements
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. Search results should come from NASA’s free APIs, for which you’ll need a free API key from https://api.nasa.gov - you do not need to enter anything more than your first name, last name, and email address (i.e. application url is not required)
 
-## Available Scripts
+- We’ve provided screenshots below of demo apps we built using the Astronomy Picture of the Day or Mars Rover Photos APIs (along with Shopify’s open source React component library: Polaris).
+- You are free to use any NASA API you like
+- You are free to use any front end framework/component library you like (or none at all!)
 
-In the project directory, you can run:
+2. Each image result should list at least a title, date of capture (ideally in earth_date) and a button to “like” that image.
+3. Each image can be “liked”, and a user should be able to undo their “like”
+4. The HTML that ends up being served client-side should be accessible and semantic (MDN reference)
 
-### `npm start`
+5. more option
+   Save likes if the user leaves or reloads the page (ok)
+   Animate the “like” action (might we suggest a heart?) (ok)
+   Add a loading state while we wait for NASA’s API to return data
+   Create shareable links for each image (ok)  
+   Add a date-picker to be able to browse photos starting from a specific date
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Your API key for voicihojun@gmail.com is:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+bbkSglcrskxOTKlFsmm1354NxeHRetmw6Ec2GbCw
+You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
 
-### `npm test`
+https://api.nasa.gov/planetary/apod?api_key=bbkSglcrskxOTKlFsmm1354NxeHRetmw6Ec2GbCw
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+TIL
 
-### `npm run build`
+1. const { data: img } = await axios.get(API_KEY);
+   //axios.get으로 받은 이미지의 data 값만 받아와서 img라는 변수에 저장한다.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+2. useEffect 사용법을 아직 확실히 잘 모르겠음.
+   startDate 입력시 입력하자마자는 startDate = "", 즉 입력했음에도 불구하고 null 값이 나옴.
+   렌더링이 한번 돌고 나서 다시 입력하면 그제서야 그 전에 입력했던 값이 나온다.
+   예) 2022-01-10 을 첫번째로 입력시 api 의 start_date = "" 으로 입력되고,
+   두번째에 다른 값을 넣었을 때(2022-01-01), 그제서야 start_date = "2022-01-10" 이 나온다.
+   이 부분이 문제임.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+3. npm i react-copy-to-clipboard
+   import { CopyToClipboard } from "react-copy-to-clipboard" 임포트 한후
+   </CopyToClipboard text={복사할 링크주소}> 로 감싸줌
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+4. npm i react-toastify
+   index.js에서
+   import { ToastContainer } from "react-toastify"; 임포트 한 후에 아래와 같이 적용해줌.
+   <React.StrictMode>
+   <App />
+   <ToastContainer />
+   </React.StrictMode>,
 
-### `npm run eject`
+사용할 js 파일에서 import { toast } from "react-toastify" 로 임포트 한후,
+toast.success("copied!")
+이렇게까지만 하면 copied 와 함꼐 엄청 큰 체크표시가 나옴.
+그래서 아래 걸 임포트 해주면 옆에서 한줄짜리 표시가 나옴.
+import "react-toastify/dist/ReactToastify.css";
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+5. HTML5 DATA 속성
+   HTML 요서의 'data-'로 시작하는 속성인데, 이것은 특정데이터를 DOM 요소에 저장해두기 위함이다.
+   데이터속성은 'data-'로 시작해야 하며, 브라우저는 이러한 데이터 속성에 어떠한 행동도 관여하지 않기 때문에 개발자는 요소에 특정한 데이터를 저장하고 싶은 경우 자유롭게 사용할 수 있다.
+   <input type="text" data-value="001" id="username">
+   var input = document.querySelector('#username')
+   console.log(input.dataset.value) ==> "001"
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+장점 : 이전과 같이 hidden으로 태그를 숨겨두고 데이터를 저장할 필요가 없다는 점. 따라서 코드가 훨씬 간결해진다.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+6. 로컬스토리지에 저장된 images.length === 0 일 때만 fetchImages를 실행하여 이미지를 새로 가져온다.
+   로컬스토리지에 저장된 이미지가 있는 경우 새로고침해도 여전히 like 등의 정보를 유지한채 가져오도록 한다.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+7. 테스트 작성
+8. 링크 눌렀을 때 copied 표시로 바뀌도록 하는 거 알아보기
+9. datepicker
+   import DatePicker from "react-datepicker";
+   import "react-datepicker/dist/react-datepicker.css";
+   <DatePicker
+   selected={startDate}
+   dateFormat="yyyy-MM-dd" // 날짜 형식
+   onChange={handleStartDate}
+   maxDate={new Date()}
+   />
+   이런 형식으로 사용되는데, onChange={date=>setStartDate(date)} 이런 형식인것을 handleStartDate로 받아서 따로 작성한것임.
 
-## Learn More
+10. ########### 하트 애니메이션 반대임
+    처음에는 하트 애니메이션이 이상없이 작동하지만 새로 고침해서 이미 하트가 좋아요 표시되어 있는 경우,
+    레드하트에서 엠티하트로 변할때 pulse가 작동됨.
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+- 수정했음. like => image.like로 해서 하트 애니메이션이 이상없이 작동되기는 하지만 레드하트인 상태에서 새로고침할 경우 pulse가 작동됨.
+- 펄스 없애고 싶음.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+11. startDate useState 없애기
+12. 날짜 넣은 후 submit 했을 때 날짜부분 text 가 없어지도록 하기
 
-### Code Splitting
+13. TypeError: images.map is not a function
+    images 내부에 알맞지 않은 data 속성이 있거나 그 데이터 속성을 잘못 사용하였을 때 에러가 나오는 듯 하다.
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+14. loading 시 로딩메시지 나오게 하기
--- src/App.js
+++ src/App.js
@@ -1,23 +1,220 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import emptyHeartImg from "./assets/emptyHeart.png";
+import heartImg from "./assets/heart.png";
+import linkImg from "./assets/link.png";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
 
 function App() {
+  const [images, setImages] = useState(
+    () => JSON.parse(localStorage.getItem("images")) || []
+  );
+  const [like, setLike] = useState(false);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [startDateStr, setStartDateStr] = useState("");
+  const [endDateStr, setEndDateStr] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [text, setText] = useState("");
+
+  const fetchImagesDefault = async () => {
+    const { data: imgs } = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=bbkSglcrskxOTKlFsmm1354NxeHRetmw6Ec2GbCw&count=5`
+    );
+    console.log("fetchImagesDefault");
+    console.log(imgs);
+    setImages(imgs);
+    setIsLoading(false);
+  };
+
+  const API = `https://api.nasa.gov/planetary/apod?api_key=bbkSglcrskxOTKlFsmm1354NxeHRetmw6Ec2GbCw&start_date=${startDateStr}&end_date=${endDateStr}`;
+  const fetchImagesWithDate = async () => {
+    // setIsLoading(true);
+    const { data: imgs } = await axios.get(API);
+    console.log("fetchImagesWithDate");
+    console.log(imgs);
+    setImages(imgs);
+
+    setStartDate(null);
+    setEndDate(null);
+    setIsLoading(false);
+  };
+
+  const onSubmit = (e) => {
+    if (startDateStr !== "" && endDateStr !== "") {
+      // null이랑 "" 은 같지 않다.
+      setIsLoading(true);
+      localStorage.clear();
+      e.preventDefault();
+
+      console.log("onSubmit");
+
+      console.log(startDateStr);
+      console.log(endDateStr);
+      fetchImagesWithDate();
+    }
+    // } else {
+    //   console.log(startDateStr);
+    //   console.log(endDateStr);
+    //   console.log("else else");
+
+    //   setText("pls enter the dates");
+    // }
+  };
+
+  localStorage.setItem("images", JSON.stringify(images));
+
+  const toggleLike = (e) => {
+    const title = e.target.dataset.value;
+    images.map((image) =>
+      title === image.title ? (image.like = !image.like) : ""
+    );
+
+    setLike(!like);
+  };
+
+  const copyUrl = () => {
+    toast.success("Copied!");
+  };
+
+  const handleStartDate = (date) => {
+    console.log("##### START: " + date);
+    setStartDate(date);
+    console.log(startDate);
+    console.log("type of startDate" + typeof startDate);
+    const dateStr = moment(date).format().substring(0, 10);
+    console.log(typeof dateStr);
+    setStartDateStr(dateStr);
+    console.log("startDateStr : " + startDateStr);
+    console.log("type of startDateStr" + typeof startDateStr);
+  };
+
+  const handleEndDate = (date) => {
+    console.log("###### END: " + date);
+    setEndDate(date);
+    console.log("endDate : " + endDate);
+    console.log("type of endDate : " + typeof endDate);
+    const dateStr = moment(date).format().substring(0, 10);
+    console.log(typeof dateStr);
+    setEndDateStr(dateStr);
+    console.log("endDateStr : " + endDateStr);
+  };
+
+  useEffect(() => {
+    console.log("USE EFFECT!!!!!!!!");
+    console.log("startDateStr : " + startDateStr);
+    console.log("endDateStr :  " + endDateStr);
+    console.log("API : " + API);
+    if (images.length === 0) {
+      fetchImagesDefault(); // 초기에는 보여줄 image 가 없으니까 그냥 5장 가져와서 보여줌.
+    } else {
+      setIsLoading(false);
+    }
+    setStartDateStr(startDateStr);
+    setEndDateStr(endDateStr);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title-form-wrapper">
+        <div className="title-wrapper">
+          <h2>spaceStagram</h2>
+          <h4>Brought to you by NASA's Astronomy Photo of the Day</h4>
+        </div>
+        <div className="form-wrapper">
+          <form onSubmit={onSubmit}>
+            <div className="calendar-container">
+              <div className="calendar-box">
+                <div>
+                  <div className="start-date">start date</div>
+                  <div>
+                    <DatePicker
+                      placeholder="yyyy-MM-dd"
+                      selected={startDate}
+                      dateFormat="yyyy-MM-dd" // 날짜 형식
+                      onChange={handleStartDate}
+                      maxDate={new Date()}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="calendar-box">
+                <div className="end-date">end date</div>
+                <div>
+                  <DatePicker
+                    selected={endDate}
+                    dateFormat="yyyy-MM-dd" // 날짜 형식
+                    onChange={handleEndDate}
+                    maxDate={new Date()}
+                  />
+                </div>
+              </div>
+            </div>
+            <button>submit</button>
+            <span id="error">{text}</span>
+          </form>
+        </div>
+      </div>
+
+      <div>
+        {isLoading ? (
+          <div className="loading-wrapper">
+            <div className="loading">
+              <span>loading...</span>
+            </div>
+          </div>
+        ) : (
+          <div className="image-content-wrapper">
+            {images.map((image) => (
+              <div>
+                <div className="image-wrapper">
+                  <img
+                    className="image"
+                    key={image.url}
+                    src={image.url}
+                    alt={image.title}
+                  />
+                </div>
+                <div className="content-wrapper">
+                  <div className="heart-link">
+                    <div className="heart">
+                      <img
+                        data-value={image.title}
+                        src={image.like ? heartImg : emptyHeartImg}
+                        className={image.like ? "heart" : "emptyHeart"}
+                        onClick={toggleLike}
+                        alt="heart"
+                      />
+                    </div>
+                    <CopyToClipboard text={image.url}>
+                      <div className="copy-url-wrapper" onClick={copyUrl}>
+                        <img
+                          src={linkImg}
+                          className="copy-url-link"
+                          alt="link"
+                        />
+                        <p>link</p>
+                      </div>
+                    </CopyToClipboard>
+                  </div>
+                  <div className="title-date">
+                    {image.title} - {image.date}
+                  </div>
+                  <div className="explanation">
+                    <p>{image.explanation}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
